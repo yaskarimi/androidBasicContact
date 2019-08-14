4b12aa40b060e23733eb045abc0f5efb2d18c4cc
ykarimi <ykarimi.pro@gmail.com>
2019-08-14 12:48:17 +0430

    first commit for drawer layout

---
+++ app/src/main/java/com/sematec/androidbasiccontact/Adapter.java
@@ -0,0 +1,17 @@
+package com.sematec.androidbasiccontact;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerView extends android.support.v7.widget.RecyclerView {
+    public RecyclerView(@NonNull Context context) {
+        super(context);
+    }
+
+    private Object ArrayList;
+    List<String> list = ArrayList;
+    
+}
